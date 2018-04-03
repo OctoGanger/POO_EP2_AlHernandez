37beaf6869fe408f5659795bb4e65657125ea7cd
OctoGanger <lannda.lathur@gmail.com>
2018-04-03 01:16:12 -0500

    Update dolar.cpp

--- dolar.cpp
+++ dolar.cpp
@@ -10,3 +10,54 @@ Dolar::Dolar(){
 Dolar::Dolar(float valor2){
     this->valor2=valor2;
 }
+
+Dolar operator +(const Dolar &v1, const Dolar &v2){
+    Dolar resultado;
+    resultado.valor2=(v1.valor2)+(v2.valor2);
+
+    return resultado;
+
+}
+Dolar operator -(const Dolar &v1, const Dolar &v2) {
+    Dolar resultado;
+    resultado.valor2= (v1.valor2) - (v2.valor2);
+
+    return resultado;
+}
+
+bool operator>(const Dolar &v1, const Dolar &v2) {
+    bool resultado;
+    resultado = (v1.valor2) > (v2.valor2);
+    return resultado;
+}
+bool operator<(const Dolar &v1, const Dolar &v2) {
+    bool resultado;
+    resultado = (v1.valor2) < (v2.valor2);
+    return resultado;
+}
+
+Peso operator++(Peso &v1, Peso &v2) {
+    Peso resultado;
+    Peso resultado2;
+    resultado.valor1 = ++(v1.valor1);
+    resultado2.valor1 = ++(v2.valor1);
+
+    return resultado;
+
+}
+
+Peso operator--(Peso &v1, Peso &v2) {
+    Peso resultado;
+    Peso resultado2;
+    resultado.valor1 = --(v1.valor1);
+    resultado2.valor1 = --(v2.valor1);
+
+    return resultado;
+}
+
+std::string Dolar::to_String() {
+    std::string valor2;
+    valor2= to_String((valor2);
+    return valor2+"USD";
+
+}
