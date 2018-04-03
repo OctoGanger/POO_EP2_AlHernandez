cc56d61c1b3e6e48e823211247c668573e6b2e64
OctoGanger <lannda.lathur@gmail.com>
2018-04-03 01:15:24 -0500

    Update peso.cpp

--- peso.cpp
+++ peso.cpp
@@ -26,6 +26,38 @@ Peso operator -(const Peso &v1, const Peso &v2) {
     return resultado;
 }
 
-bool operator>(const Peso &v1, const Peso &v2){
+bool operator>(const Peso &v1, const Peso &v2) {
+    bool resultado;
+    resultado = (v1.valor1) > (v2.valor1);
+    return resultado;
+}
+bool operator<(const Peso &v1, const Peso &v2) {
+    bool resultado;
+    resultado = (v1.valor1) < (v2.valor1);
+    return resultado;
+}
 
+Peso operator++(Peso &v1, Peso &v2) {
     Peso resultado;
+    Peso resultado2;
+    resultado.valor1 = ++(v1.valor1);
+    resultado2.valor1 = ++(v2.valor1);
+
+    return resultado;
+
+}
+
+Peso operator--(Peso &v1, Peso &v2) {
+    Peso resultado;
+    Peso resultado2;
+    resultado.valor1 = --(v1.valor1);
+    resultado2.valor1 = --(v2.valor1);
+
+    return resultado;
+
+}
+std::string Peso::to_String() {
+    std::string valor1;
+    valor1 = to_String((valor1);
+    return valor1 + "MXN";
+};
